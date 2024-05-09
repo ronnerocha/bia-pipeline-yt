17c7d32fe8effa4b797278bd77d47f11d0333522
Ronne Rocha <ronnerocha@hotmail.com>
2024-05-08 22:31:09 -0300

    alteração de ronne

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -10,8 +10,8 @@ const Header = ({ title, onAdd, showAdd }) => {
       {/* <h3>process.env.EMAIL_ALUNO</h3> */}
       {location.pathname === "/" && (
         <Button
-          color={showAdd ? "black" : "blue"}
-          text={showAdd ? "Close" : "Add+"}
+          color={showAdd ? "black" : "red"}
+          text={showAdd ? "Fechar" : "Adicionar+"}
           onClick={onAdd}
         />
       )}
@@ -20,7 +20,7 @@ const Header = ({ title, onAdd, showAdd }) => {
 };
 
 Header.defaultProps = {
-  title: "BIA 2024 RONNE",
+  title: "BIA 2024 RONNE Pipeline",
 };
 
 export default Header;
